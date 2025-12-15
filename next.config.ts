aa5b2d5119beed3b9d9dc4cae33a77ace7e2e57e
rehman2711 <abdul.r@jaguarsolutions.co.in>
2025-12-15 22:48:06 +0530

    Next config updated cause warning of depreciation

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "gearshift2images.s3.eu-north-1.amazonaws.com"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gearshift2images.s3.eu-north-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
